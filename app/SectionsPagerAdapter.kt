12e1681e7560ff5a7426d994b2ac5fefcc616dc9
benayalaskar <benayalaskar2002@gmail.com>
2024-03-20 09:57:19 +0700

    Initial commit

---
+++ app/SectionsPagerAdapter.kt
@@ -0,0 +1,6 @@
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
+}
